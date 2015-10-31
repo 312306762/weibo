0dae4b3efff272a1b213b0237342b392baacb1b9
manager <manager@qq.com>
2015-10-31 20:29:50 +0800

    初始化结构

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by kk on 15/10/31.
+//  Copyright © 2015年 xmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
